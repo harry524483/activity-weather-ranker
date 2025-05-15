43c22862e0219ca0bff9c043826c0033f7f7cca5
harry524483 <astroharinder.singh@gmail.com>
2025-05-15 23:25:37 +0100

    feat(api): integrate Apollo Server with Express, add configuration, and update dependencies

--- apps/api/codegen.ts
+++ apps/api/codegen.ts
@@ -1,16 +1,16 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'libs/shared/src/models/*.graphql',
-    generates: {
-        'apps/api/src/__generated__/resolvers.ts': {
-            plugins: ['add', 'typescript-resolvers'],
-            config: {
-                useIndexSignature: true,
-                content: 'import * as types from "@activity-weather-ranker/shared"',
-                namespacedImportName: 'types',
-            },
-        },
+  schema: 'libs/shared/src/models/*.graphql',
+  generates: {
+    'apps/api/src/__generated__/resolvers.ts': {
+      plugins: ['add', 'typescript-resolvers'],
+      config: {
+        useIndexSignature: true,
+        content: 'import * as types from "@activity-weather-ranker/shared"',
+        namespacedImportName: 'types',
+      },
     },
+  },
 };
 export default config;
