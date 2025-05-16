69c1513bf03a0e04321dd73cf69edd9cd74f5187
harry524483 <astroharinder.singh@gmail.com>
2025-05-17 00:39:54 +0100

    refactor(frontend): reorganize GraphQL operations and update file structure for generated types

--- apps/frontend/codegen.ts
+++ apps/frontend/codegen.ts
@@ -4,7 +4,7 @@ const config: CodegenConfig = {
   schema: 'libs/shared/src/models/*.graphql',
   documents: ['apps/frontend/src/common/graphql/**/*.graphql'],
   generates: {
-    'apps/frontend/src/common/__generated__/operations.ts': {
+    'apps/frontend/src/common/graphql/__generated__/operations.ts': {
       plugins: ['add', 'typescript-operations', 'typescript-react-apollo'],
       config: {
         useIndexSignature: true,
